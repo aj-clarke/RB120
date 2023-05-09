1c3cacb2f9586760e6b62792b05f1164496ab494
AJ Clarke <ajclarke81@gmail.com>
2023-05-08 22:15:10 -0400

    Adding lesson 2 polymorphism and encapsulation examples

---
+++ lesson_2/06_lecture_polymorphism_n_encapsulation/polymorphism_via_ducktyping.rb
@@ -0,0 +1,86 @@
+# DO NOT do it this way
+
+class Wedding
+  attr_reader :guests, :flowers, :songs
+
+  def prepare(preparers)
+    preparers.each do |preparer|
+      case preparer
+      when Chef
+        preparer.prepare_food(guests)
+      when Decorator
+        preparer.decorate_place(flowers)
+      when Musician
+        preparer.prepare_performance(songs)
+      end
+    end
+  end
+end
+
+class Chef
+  def prepare_food(guests)
+    # implementation
+  end
+end
+
+class Decorator
+  def decorate_place(flowers)
+    # implementation
+  end
+end
+
+class Musician
+  def prepare_performance(songs)
+    #implementation
+  end
+end
+
+# Why not? Answer:
+=begin
+Preparer method has too many dependencies. If anything were to be changed or if
+any additional `preparers` were needed, then the `Wedding#prepare` method 
+becomes very messy and hard to maintain.
+=end
+
+
+# The RIGHT way ----------------------------------------------------------
+
+class Wedding
+  attr_reader :guests, :flowers, :songs
+
+  def prepare(preparers)
+    preparers.each do |preparer|
+      preparer.prepare_wedding(self)
+    end
+  end
+end
+
+class Chef
+  def prepare_wedding(wedding)
+    prepare_food(wedding.guests)
+  end
+
+  def prepare_food(guests)
+    #implementation
+  end
+end
+
+class Decorator
+  def prepare_wedding(wedding)
+    decorate_place(wedding.flowers)
+  end
+
+  def decorate_place(flowers)
+    # implementation
+  end
+end
+
+class Musician
+  def prepare_wedding(wedding)
+    prepare_performance(wedding.songs)
+  end
+
+  def prepare_performance(songs)
+    #implementation
+  end
+end
